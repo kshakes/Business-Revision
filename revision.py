a353a1bcfc101052b8e77ff7e1f9631ef6d39f36
Shakezk <kyleshakes04@gmail.com>
2024-05-03 18:16:01 +0100

    Outline of NPV function created

--- revision.py
+++ revision.py
@@ -1,8 +1,8 @@
 import random
+import math
 
 def calculateARR():
     cashFlows = []
-
     initInvestment = random.randint(8, 15) * 1000 # Generate a random initial investment
     amountOfCashFlows = random.randint(3,5) # Generate a random amount of cash flowing years
     print ("Initial Investment: £", initInvestment)
@@ -29,3 +29,21 @@ def calculateARR():
     print ("Avg Investment: £", avgInvestment)
     print (round(accountingRateOfReturn, 2))
 
+def NetPresentValue():
+    cashFlows = []
+    discountRate = random.randint(5, 15) / 100
+    initInvestment = random.randint(20, 40) * 1000
+    amountOfCashFlows = random.randint(3,5)
+    print ("Initial Investment: £", initInvestment)
+    print (discountRate)
+    
+    npv = 0 - initInvestment
+    print ("NPV after initial investment: £", npv)
+
+    for year in range (amountOfCashFlows): # Loops through the amount of cash flowing years
+        cashFlows.append(random.randint(11, 18) * 1000) # Append a random cash flow
+        print ("Year ", year+1, ": ", cashFlows[year]) # Print the cash flows
+        npv += cashFlows[year] / math.pow(1+discountRate, year+1)
+    print (npv)
+
+NetPresentValue()
