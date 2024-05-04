aa2a9aed7794e064d8b4bfa1f9dd2825c8d44d10
Shakezk <kyleshakes04@gmail.com>
2024-05-04 14:33:17 +0100

    Added table with "PrettyTables"
    Added table with net profit adjustment with PrettyTables

--- revision.py
+++ revision.py
@@ -1,5 +1,6 @@
 import random
 import math
+from prettytable import PrettyTable 
 
 def calculateARR():
     cashFlows = []
@@ -49,20 +50,20 @@ def NetPresentValue():
     print (round(npv, 2))
 
 def isAnswerCorrect(usersAnswer, answer):
-    if usersAnswer == round(answer, 2): # Check if its correct
+    if usersAnswer == answer: # Check if its correct
         return "Correct!"
     else:
         return "Incorrect :("
 
 def partnershipAccounting():
     # capA = Capital Account interest rate
-    capAInterestRate = random.randint(5,10) / 100
+    capAInterestRate = round(random.randint(5,10) / 100, 1)
     #Salaries [0] = A, [1] = B, [2] = C
     salaries = [random.randint(10,50) * 1000, random.randint(30,60) * 1000, random.randint(30, 80) * 1000]
     #Profit Share
     profitShare = [random.randint(1,5),random.randint(1,5),random.randint(1,5)]
     #Interest rate on drawings
-    drawingInterestRate = random.randint(5,15) / 100
+    drawingInterestRate = round(random.randint(5,15) / 100, 1)
 
     #Drawing Amount
     drawingAmount = [random.randint(50,80) * 1000, random.randint(40,80) * 1000, random.randint(30,80) * 1000]
@@ -76,37 +77,66 @@ def partnershipAccounting():
     #Net Profit
     netProfit = random.randint(150,250) * 1000
 
+    #Introduce the question
+    print ("A, B, C are in partnership and their agreements include:\ni. Interest at ", capAInterestRate * 100, "% charged on capital accounts\nii. No interest on current accounts\niii. A recieves a salary of £",salaries[0], "\niv. B recieves a salary of £", salaries[1], "\nv. C recieves a salary of £", salaries[2], "vi. Profit sharing is ", profitShare[0], ":", profitShare[1], ":", profitShare[2], "\nvii. Interest on drawings was charged at ", drawingInterestRate*100, "% per annum")
+    print ("During the year, A made drawings of £", drawingAmount[0], ", B made drawings of £", drawingAmount[1], ", C made drawings of £", drawingAmount[2], ". Assume these are all made on 1/1/2023")
+    print ("A, B, and C's current accounts at 31/12/2022 were £", currentAccountBalances[0], ", £", currentAccountBalances[1], ", £", currentAccountBalances[2], " respectively")
+    print ("A, B, and C's capital accounts at 31/12/2022 were £", capitalAccountBalances[0], ", £", capitalAccountBalances[1], ", £", capitalAccountBalances[2], " respectively")
+    print ("Net Profit before interest and salaries was  £", netProfit)
+    print ("\n\nRequired:-\na. Profit and loss appropration account for the year ended 31/12/2023\nb. Partners current accounts as at 31/12/2023\nGive your final answer of the partners current acounts separated by commas. For example -> 10,000, 20,000, 30,000")
+
+    usersAnswer = input("Answer -> ")
+
     #Step 1, Net Profit Adjustments to find share of profit
-    print ("Net Profit: £", netProfit)
-    print ("Capital Account Interest Rate: ", capAInterestRate, "%")
-    print ("Salaries: ", salaries)
-    print ("Profit sharing ratio: ", profitShare)
-    print ("Drawing interest rate: ", drawingInterestRate, "%")
-    print ("Drawing Amount: ", drawingAmount)
-    print ("Current Accounts: ", currentAccountBalances)
-    print ("Capital Accounts: ", capitalAccountBalances)
+    shareOfProfit = [] # Create an array to append the partners share of profits
 
-    shareOfProfit = []
+    netProfitAdj = netProfit + (sum(drawingAmount) * drawingInterestRate) - sum(salaries) - (sum(capitalAccountBalances) * capAInterestRate) # Calculating net profit adjustments by adding the interest on drawings and taking away salaries and interest on capital
 
-    netProfitAdj = netProfit + (sum(drawingAmount) * drawingInterestRate) - sum(salaries) - (sum(capitalAccountBalances) * capAInterestRate)
+    shareOfProfit.append(round(netProfitAdj * (profitShare[0] / sum(profitShare)), 2))
+    shareOfProfit.append(round(netProfitAdj * (profitShare[1] / sum(profitShare)), 2))
+    shareOfProfit.append(round(netProfitAdj * (profitShare[2] / sum(profitShare)), 2))
 
-    shareOfProfit.append(netProfitAdj * (profitShare[0] / sum(profitShare)))
-    shareOfProfit.append(netProfitAdj * (profitShare[1] / sum(profitShare)))
-    shareOfProfit.append(netProfitAdj * (profitShare[2] / sum(profitShare)))
+    NetProfitTable = PrettyTable(["Net Profit Adjustments", "£"])
+    NetProfitTable.add_row(["Net Profit", netProfit])
+    NetProfitTable.add_row(["Drawing Interest", ""])  # Add an empty string for the second column
+    NetProfitTable.add_row(["A", drawingAmount[0] * drawingInterestRate])
+    NetProfitTable.add_row(["B", drawingAmount[1] * drawingInterestRate])
+    NetProfitTable.add_row(["C", drawingAmount[2] * drawingInterestRate])
+    NetProfitTable.add_row(["", netProfit + (sum(drawingAmount) * drawingInterestRate)])
+    NetProfitTable.add_row(["Salary", ""])
+    NetProfitTable.add_row(["A", salaries[0]])
+    NetProfitTable.add_row(["B", salaries[1]])
+    NetProfitTable.add_row(["C", salaries[2]])
+    NetProfitTable.add_row(["Interest on Capital", ""])
+    NetProfitTable.add_row(["A", capitalAccountBalances[0] * capAInterestRate])
+    NetProfitTable.add_row(["B", capitalAccountBalances[1] * capAInterestRate])
+    NetProfitTable.add_row(["C", capitalAccountBalances[2] * capAInterestRate])
+    NetProfitTable.add_row(["", netProfit + (sum(drawingAmount) * drawingInterestRate) - sum(salaries) - sum(capitalAccountBalances) * capAInterestRate])
+    NetProfitTable.add_row(["Share of Capital", ""])
+    NetProfitTable.add_row(["A", shareOfProfit[0]])
+    NetProfitTable.add_row(["B", shareOfProfit[1]])
+    NetProfitTable.add_row(["C", shareOfProfit[2]])
 
-    for i in shareOfProfit:
-        print ("£", i)
-    
     #Profit and Loss Appropriation Account:
     for index, partner in enumerate(shareOfProfit):
         partner = partner - (drawingAmount[index] * drawingInterestRate) + salaries[index] + (capitalAccountBalances[index] * capAInterestRate)
         shareOfProfit[index] = partner
-    print (shareOfProfit)
+    print ("Share of Profit:\nA -> £", shareOfProfit[0], "\nB -> £", shareOfProfit[1], "\nC -> £", shareOfProfit[2])
 
     #Partner Current Account:
     for index, partner in enumerate(currentAccountBalances):
         partner = partner + shareOfProfit[index] - drawingAmount[index]
         currentAccountBalances[index] = partner
-    print (currentAccountBalances)
+    print ("Current Accounts:\nA -> £", currentAccountBalances[0], "\nB -> £", currentAccountBalances[1], "\nC -> £", currentAccountBalances[2])
+
+    answer = "{:,.0f}, {:,.0f}, {:,.0f}".format(currentAccountBalances[0], currentAccountBalances[1], currentAccountBalances[2])
+
+    
+
+    print ("Answer -> ", answer)
+
+    print(isAnswerCorrect(usersAnswer, str(answer)))
+
+    print (NetProfitTable)
 
 partnershipAccounting()
